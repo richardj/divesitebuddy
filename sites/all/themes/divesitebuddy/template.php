022ffeda09f689b93b5bffbb436457f6a89d8e06
Richard Japenga <treurmars@gmail.com>
2013-02-27 16:21:34 +0100

    updates to the theme

--- sites/all/themes/divesitebuddy/template.php
+++ sites/all/themes/divesitebuddy/template.php
@@ -21,6 +21,30 @@
  * 2. Uncomment the required function to use.
  */
 
+/**
+ * Override of theme('textarea').
+ * Deprecate misc/textarea.js in favor of using the 'resize' CSS3 property.
+ */
+function divesitebuddy_textarea($vars) {
+  $element = $vars['element'];
+  element_set_attributes($element, array('id', 'name', 'cols', 'rows'));
+  _form_set_class($element, array('form-textarea'));
+ 
+  $wrapper_attributes = array(
+    'class' => array('form-textarea-wrapper'),
+  );
+ 
+  // Add resizable behavior.
+  if (!empty($element['#resizable'])) {
+    $wrapper_attributes['class'][] = 'resizable';
+  }
+ 
+  $output = '<div' . drupal_attributes($wrapper_attributes) . '>';
+  $output .= '<textarea' . drupal_attributes($element['#attributes']) . '>' . check_plain($element['#value']) . '</textarea>';
+  $output .= '</div>';
+ 
+  return $output;
+}
 
 /**
  * Preprocess variables for the html template.
